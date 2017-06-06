fbede6b5c0fbf8a204cd51400f1b816005357db3
jonah <jlangli1@swarthmore.edu>
2017-06-06 00:59:54 -0400

    3 POINTS

--- functions/makeplane2.m
+++ functions/makeplane2.m
@@ -1,4 +1,4 @@
-function clippingPlane = makeplane2(C1,z,x1,y1,z1,r,flip)
+function clippingPlane = makeplane2(C1, xs, ys, zs ,flip, z)
 %This function can be used to make a clipping plane for a slice of a 3D
 %image.
 %
@@ -8,7 +8,16 @@ function clippingPlane = makeplane2(C1,z,x1,y1,z1,r,flip)
 %Date: 6/2/17
 %Contact: colgan.william@gmail.com
 [x,y] = size(C1);
-clippingPlane = double(zeros(x,y));
+clippingPlane = double(ones(x,y))*.5;
+% see http://www.math.cornell.edu/~froh/231f08e1a.pdf
+allPoints = [xs*x; ys*y; zs];
+% sub1 = a(:, 1) - a(:, 2)
+% sub2 = a(:, 3) - a(:, 2)
+coeffs = cross(allPoints(:, 1) - allPoints(:, 2), allPoints(:, 3) - allPoints(:, 2));
+r = sum(coeffs .*allPoints(:, 1));
+x1 = coeffs(1);
+y1 = coeffs(2);
+z1 = coeffs(3);
 for i = 1:x
     for j = 1:y
         if flip == 1
@@ -16,7 +25,8 @@ for i = 1:x
                 clippingPlane(i,j) = 1;
             end
         else
-            if(i*x1+j*y1+z*z1<r)
+%             if(i*x1+j*y1+z*z1<r)
+           if(i*x1+j*y1+z*z1<r)
                 clippingPlane(i,j) = 1;
             end
         end
